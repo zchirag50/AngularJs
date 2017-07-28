260c9d84effa0e78a75cc381cdbbabdecf891080
zchirag50 <chiragpatel08bce050@gmail.com>
2017-07-28 18:25:04 +0530

    Delete app.js

--- TypeScriptAngularJs1.6/app.js
+++
@@ -1,6 +0,0 @@
-define(["require", "exports", "./controller"], function (require, exports, controller_1) {
-    var app = angular.module('app', []);
-    angular.module('app')
-        .controller("EmployeeCtrl", controller_1.EmployeeCtrl);
-});
-//# sourceMappingURL=app.js.map
