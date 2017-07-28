82fc44a28ef9074c437e2dccf9a1a53286bb315e
zchirag50 <chiragpatel08bce050@gmail.com>
2017-07-28 18:37:13 +0530

    Update app.ts

--- TypeScriptAngularJs1.6/app.ts
+++ TypeScriptAngularJs1.6/app.ts
@@ -1,8 +1,10 @@
 import {EmployeeCtrl} from "./controller"
 import {myDirective} from "./myDirective"
+import { myComponent } from './myComponent';
 
 var app = angular.module('app',[]);
 
 angular.module('app')
-    .directive(myDirective.id,myDirective.instance)
-    .controller("EmployeeCtrl",EmployeeCtrl);
+    .directive(myDirective.id, myDirective.instance)
+    .controller("EmployeeCtrl",EmployeeCtrl)
+    .component(myComponent.id, myComponent.instance());
